65ee54446fabae6be9e278eaa5fcbddbf1ed0f66
gion4s <gionas.bonardi@gmail.com>
2019-04-20 13:23:22 +0200

    Immagine che cambia

--- MeteoApp_with_MVVM/MeteoApp/Views/MeteoItemPage.xaml.cs
+++ MeteoApp_with_MVVM/MeteoApp/Views/MeteoItemPage.xaml.cs
@@ -10,6 +10,31 @@ namespace MeteoApp
         public MeteoItemPage()
         {
             InitializeComponent();
+
+            var temp = BindingContext as MeteoItemViewModel;
+            Console.WriteLine(temp);
+
+            /*switch (temp.City.weatherID / 100)
+            {
+                case 2:
+                    MainImage.Source = "thunderstorm.png";
+                    break;
+                case 3:
+                    MainImage.Source = "rain.png";
+                    break;
+                case 5:
+                    MainImage.Source = "snowerrain.png";
+                    break;
+                case 6:
+                    MainImage.Source = "mist.png";
+                    break;
+                case 7:
+                    MainImage.Source = "snow.png";
+                    break;
+                default:
+                    Console.WriteLine("Default case");
+                    break;
+            }*/
         }
     }
 }
